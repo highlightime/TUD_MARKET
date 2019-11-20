0d4d9b4c8782665db213248a8455a8e96fe10ff0
byun618 <byun618@gmail.com>
2019-11-20 20:03:15 +0000

    add file

--- view.php
+++ view.php
@@ -121,6 +121,30 @@
             $result = $connect->query($query);
         ?>
 
+        <div class="reply_view">
+            <h3>Reply List</h3>
+            <?php
+                $query = "select * from reply where board_id = $board_id";
+                $result = mysqli_query($connect, $query);
+                while($rows = mysqli_fetch_assoc($result)){
+            ?>
+            <div class="dap_lo">
+                <div><b><?php echo $rows['author'];?></b></div>
+                <div class="dap_to comt_edit"><?php echo $rows['content'];?></div>
+                <div class="rep_me dap_to"><?php echo $rows['date']?></div>
+            </div>
+        </div>
+        <?php } ?>
+
+        <div class="dap_ins">
+            <div style="margin-top:10px;">
+                <form method="post" action="reply_action.php">
+                    <input type="hidden" name="board_id" class="bno" value="<?php echo $board_id?>">
+                    <textarea name="content" class="reply_content" id="re_content"></textarea>
+                 <input type="submit" value="Reply">
+                </form>
+            </div>
+        </div>
         <!-- MODIFY & DELETE -->
         <div class="view_btn">
             <button class="view_btn1" onclick="location.href='./index.php'">To List</button>
@@ -135,3 +159,66 @@
         <?php
     }
     ?>
+<style>
+    /* 댓글 */
+    .reply_view {
+        width:900px;
+        margin-top:100px;
+        word-break:break-all;
+    }
+    .dap_lo {
+        font-size: 14px;
+        padding:10px 0 15px 0;
+        border-bottom: solid 1px gray;
+    }
+    .dap_to {
+        margin-top:5px;
+    }
+    .rep_me {
+        font-size:12px;
+    }
+    .rep_me ul li {
+        float:left;
+        width: 30px;
+    }
+    .dat_delete {
+        display: none;
+    }
+    .dat_edit {
+        display:none;
+    }
+    .dap_sm {
+        position: absolute;
+        top: 10px;
+    }
+    .dap_edit_t{
+        width:520px;
+        height:70px;
+        position: absolute;
+        top: 40px;
+    }
+    .re_mo_bt {
+        position: absolute;
+        top:40px;
+        right: 5px;
+        width: 90px;
+        height: 72px;
+    }
+    #re_content {
+        width:700px;
+        height: 56px;
+    }
+    .dap_ins {
+        margin-top:50px;
+    }
+    .re_bt {
+        position: absolute;
+        width:100px;
+        height:56px;
+        font-size:16px;
+        margin-left: 10px;
+    }
+    #foot_box {
+        height: 50px;
+    }
+</style>
