38731581922120b56a8d27958e6c938b8ee6f950
yunobro <58005728+yunobro@users.noreply.github.com>
2019-11-21 14:33:31 +0000

    Edit signup

--- signup.php
+++ signup.php
@@ -1,27 +1,31 @@
 <?php
-    $connect = mysqli_connect('localhost', 'root', '', 'Second_market') or die ("connect fail");
-
-    $id = $_GET['id_text'];
-    echo $id;
-
-  /*  $query = "insert into member values ()";
-
-    $result = $connect -> query($query);
-
-    if($result) {
-        ?>
-        <script>
-            alert("Signed up!!");
-            location.replace("../Login/login.html");
-        </script>
-        <?php
-    }else{
-        ?>
-        <script>
-            alert("fail");
-        </script>
-        <?php
-    }
-    mysqli_close($connect);*/
-
+ 
+        $connect = mysqli_connect('localhost', '', '', '') or die("fail");
+ 
+        $id=$_GET[id];
+        $pw=$_GET[pw];
+        $email=$_GET[email];
+        $addr=$_GET[addr1_text];
+        $city=$_GET[city_text];
+ 
+        $date = date('Y-m-d H:i:s');
+ 
+        $query = "insert into member (id, pw, mail, date, addr, city, permit) values ('$id', '$pw', '$email', '$addr', '$city', '$date', 0)";
+ 
+        $result = $connect->query($query);
+ 
+        if($result) {
+        ?>      <script>
+                alert('가입 되었습니다.');
+                location.replace("./login_action.php");
+                </script>
+ 
+<?php   }
+        else{
+?>              <script>                   
+                        alert("fail");
+                </script>
+<?php   }
+ 
+        mysqli_close($connect);
 ?>
